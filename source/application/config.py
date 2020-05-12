2b4cfda4aaa699a86c444235313de8822320ecad
ahmadhmirza <ahmadhasanmirza@gmail.com>
2020-05-12 11:43:02 +0200

    path to tf-model

--- source/application/config.py
+++ source/application/config.py
@@ -1,3 +1,8 @@
+import os
+#Get the path to the current script:
+#This will facilitate in geting the rest of the path programatically.
+SCRIPT_PATH = os.path.dirname(__file__)
+
 MAIN_WINDOW_TITLE = "VEd²" # Visual Evaluation of Drone Data - VEDD
 
 #Initial UI Size
@@ -7,5 +12,14 @@ WIDTH   =   500
 HEIGHT  =   500
 
 #Push Button Strings
-
 LOAD_DATA = "Select File"
+
+
+#ASSETS
+ASSETS_DIR_NAME         = "assets"
+TENSOR_FLOW_MODEL_NAME  = "frozen_inference_graph.pb"
+
+#Path to assets directory
+ASSETS_DIR          = os.path.join(SCRIPT_PATH,ASSETS_DIR_NAME)
+#Path to trained_inference_graph
+TENSOR_FLOW_MODEL   = os.path.join(ASSETS_DIR,TENSOR_FLOW_MODEL_NAME)
