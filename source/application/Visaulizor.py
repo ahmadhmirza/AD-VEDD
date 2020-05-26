6c8cd45ac2ada98fa38b91c53ccb6166dd8a77e5
ahmadhmirza <ahmadhasanmirza@gmail.com>
2020-05-26 20:51:32 +0200

    exception handling

--- source/application/Visaulizor.py
+++ source/application/Visaulizor.py
@@ -1,6 +1,6 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from pyexiv2 import Image
-from UI import *
+from UI_Layout import *
 import os
 import re
 from ui_Dialog import *
@@ -10,27 +10,36 @@ class Visualisor(UI,Ui_Dialog):
                 self.path = path
                 return(self.path)
         def LoadMetadata(self, path):
-                i= Image(path, encoding='utf-8')
-                i.modify_exif({'Exif.Photo.MakerNote': 'test'})
-                ex = i.read_exif()
-                print(len(ex))
-                xm = i.read_xmp()
-                self.Metadata = {**ex, **xm}    
-                self.tableLength = len(self.Metadata)
-                print (self.tableLength)
-                del ex['Exif.Image.XPComment']
-                self.Metadata_table.setRowCount(self.tableLength)
-                # self.PopulateTable(self.Metadata)
-                self.p = 'Exif.'
-                for i, (k, v) in enumerate(self.Metadata.items()):
-                        k= re.sub('Exif.','',k)  
-                        k= re.sub('Xmp.','',k)                              
-                        # print(i, k, v)
-                        self.newitem1 = QtWidgets.QTableWidgetItem(k)
-                        self.newitem2 = QtWidgets.QTableWidgetItem(v)
-                        self.Metadata_table.setItem(i, 0, self.newitem1)
-                        self.Metadata_table.setItem(i, 1, self.newitem2)   
-                return self.Metadata
+                try:
+                        i= Image(path, encoding='utf-8')
+                        i.modify_exif({'Exif.Photo.MakerNote': 'test'})
+                        ex = i.read_exif()
+                        print("Metadata entries found:" + str(len(ex)))
+                        xm = i.read_xmp()
+                        self.Metadata = {**ex, **xm}    
+                        self.tableLength = len(self.Metadata)
+                        print (self.tableLength)
+                        del ex['Exif.Image.XPComment']
+                        self.Metadata_table.setRowCount(self.tableLength)
+                        # self.PopulateTable(self.Metadata)
+                        self.p = 'Exif.'
+                        for i, (k, v) in enumerate(self.Metadata.items()):
+                                k= re.sub('Exif.','',k)  
+                                k= re.sub('Xmp.','',k)                              
+                                # print(i, k, v)
+                                self.newitem1 = QtWidgets.QTableWidgetItem(k)
+                                self.newitem2 = QtWidgets.QTableWidgetItem(v)
+                                self.Metadata_table.setItem(i, 0, self.newitem1)
+                                self.Metadata_table.setItem(i, 1, self.newitem2)   
+                        return True
+                except Exception as e:
+                        print("visualizer: " + str(e))
+
+                        self.Metadata_table.setRowCount(1)
+                        self.Metadata_table.setColumnCount(2)
+                        self.Metadata_table.setItem(1,1, QtWidgets.QTableWidgetItem("ERROR"))
+                        self.Metadata_table.setItem(1,1, QtWidgets.QTableWidgetItem(str(e)))
+                        return str(e)
         def one(self):
                 if self.Metadata_table:
                         self.add_new.setEnabled(True)
