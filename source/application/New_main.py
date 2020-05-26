f3113cf7710726623d7929a56680d8d71865a6f3
ahmadhmirza <ahmadhasanmirza@gmail.com>
2020-05-26 21:50:32 +0200

    image processing function

--- source/application/New_main.py
+++ source/application/New_main.py
@@ -1,23 +1,24 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+# Library Imports
 import cv2
 import os
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import QImage
+# Internal modules import
 import Config as APP_CONFIG
 from Visualizer import *
 from UI_Layout import *
-from PyQt5.QtGui import QImage
-from ui_Dialog import *
+from UI_Dialog import *
+# Image processing files
+import VehicleDetection as vd
+import LaneDetection as ld
 
 class ImageLoader(UI,Ui_Dialog):
     def __init__(self,MainWindow):
         UI.__init__(self,MainWindow)
-        self.title          =   APP_CONFIG.MAIN_WINDOW_TITLE
-        self.left           =   APP_CONFIG.LEFT
-        self.top            =   APP_CONFIG.TOP
-        self.width          =   APP_CONFIG.WIDTH
-        self.height         =   APP_CONFIG.HEIGHT
-        self.img_resolution = (APP_CONFIG.WIDTH, APP_CONFIG.HEIGHT)
+        # Signals for push buttons and menu items
         self.loadImage.clicked.connect(self.getImage) 
-
+        self.analyseImage.clicked.connect(self.processImage)
+        self.actionExport_Image.triggered.connect(self.saveImage)
     def displayStatus(self,statusMessage):
         MainWindow.statusBar().showMessage(statusMessage)
 
@@ -25,26 +26,38 @@ class ImageLoader(UI,Ui_Dialog):
         self.fname = QtWidgets.QFileDialog.getOpenFileName(MainWindow, 'Open file','C:\\', "Image files (*.jpg *.gif)")
         try:
             self.imagePath = self.fname[0]
-            self.img = cv2.imread(self.imagePath)
-            self.img = cv2.resize(self.img, self.img_resolution )
+            self.image = cv2.imread(self.imagePath)
+            height, width, channels = self.image.shape
+            print("Resolution: " + str(height)+", " + str(width))
+
+            #Read metadata from the image
             Visualisor.getImagePath(self, self.imagePath)
             status = Visualisor.LoadMetadata(self, self.imagePath)
             if status !=True:
-                self.displayStatus("ERROR: Unable to read meta-data from image: " + status)
-            # Visualisor.one(self)
-            self.displayImageFromArray()      
-            return True
+                if self.displayImageFromArray():
+                    self.displayStatus("Image loaded and ready for analysis, Error(s) encountered while reading exif-data from image: " + status) 
+                    return True
+                else:
+                    self.displayStatus("Unable to load the image. ")
+                    return False
+            else:
+                #Draw the image on canvas
+                self.displayImageFromArray()      
+                self.displayStatus('Image loaded and ready for analysis. Image resolution: '+ str(height)+" x " + str(width))
+                return True
         except Exception as e:
             print(str(e))
+            self.displayStatus('Error loading image.')
             return False
+
     def displayImageFromArray(self):
         print("INFO: MAIN: Drawing image on canvas.")
         try:
             pixmap = QtGui.QPixmap(self.imagePath)
-            height, width, channel = self.img.shape
+            height, width, channel = self.image.shape
             bytesPerLine = 3 * width
             #Create a QImage object from the numpy image array
-            qImg = QImage(self.img.data, width, height, bytesPerLine, QImage.Format_RGB888)
+            qImg = QImage(self.image.data, width, height, bytesPerLine, QImage.Format_RGB888)
             #Create a QPixmap to be displayed on the label widget
             pix = QtGui.QPixmap(qImg)
             pix = pixmap.scaledToWidth(APP_CONFIG.IMG_WIDTH*1.5)
@@ -56,6 +69,43 @@ class ImageLoader(UI,Ui_Dialog):
             print(str(e)) 
             return False
 
+    def processImage(self):
+        try:
+            # Make a copy of the original image
+            img = self.image
+            print("INFO: MAIN: Image read for analysis.")
+            print("INFO: MAIN: Processing image...")
+            self.displayStatus('Starting image analysis...')
+            # Perform detections
+            procImg,laneCoordinates = ld.detectLanesWhite(img)
+            procImg,vehicleMetaData = vd.detectVehicles(procImg)
+            print("INFO: MAIN: Analysis done!")
+            print("Lane Data:")
+            print(laneCoordinates)
+            print("Vehicle Data:")
+            print(vehicleMetaData)
+            self.displayStatus('Image analysis finished.')
+            # Update the original image with the processed image
+            self.image = procImg
+            self.displayImageFromArray()
+            print("INFO: MAIN: Process finished.")
+            self.displayStatus('Image analysis results on canvas.')
+            return True
+        except Exception as e:
+            print("ERROR: MAIN:" + str(e))
+            self.displayStatus('Error(s) encountered while processing image.')
+            return False
+
+    def saveImage(self):
+        try:
+            fname = QtWidgets.QFileDialog.getSaveFileName(MainWindow, 'Save file','VEDD_outImage', "Image files (*.jpg)")
+            filePath = fname[0]+".jpg"
+            cv2.imwrite(filePath,self.image)
+            self.displayStatus('File saved: ' + filePath)
+        except Exception as e:
+            print(str(e))
+            self.displayStatus('Error(s) encountered while saving image.')
+            return False
         
         
 if __name__ == "__main__":
